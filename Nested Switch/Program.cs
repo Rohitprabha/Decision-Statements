cede272e4b662f10d9214c9c1f7874a7448a49b6
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 20:47:39 +0530

    Add files via upload

---
+++ Nested Switch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Nested_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Men = "Women";
+            string MartialStatus = "UnMarried";
+            switch (Men)
+            {
+                case "Men":
+                    switch (MartialStatus)
+                    {
+                        case "Married":
+                            Console.WriteLine("The Person is Men and Married");
+                            break;
+                        case "UnMarried":
+                            Console.WriteLine("The Person is Men and UnMarried");
+                            break;
+                    }
+                    break;
+                case "Women":
+                    switch (MartialStatus)
+                    {
+                        case "Married":
+                            Console.WriteLine("The Person is Women and Married");
+                            break;
+                        case "UnMarried":
+                            Console.WriteLine("The Person is Women and UnMarried");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("The person is not  Men or Women");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
