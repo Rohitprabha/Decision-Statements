90091d2c93c121e81f3260226566683d8f87a387
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 20:09:58 +0530

    Add files via upload

---
+++ if_ifelse_nested_if/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace If_Ifelse_nested_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("The 1st Number is the largest");
+                }
+                else
+                {
+                    Console.Write("The 3rd Number is the largest");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.Write("The 2nd Number is the largest");
+            }
+            else
+            {
+                Console.Write("The 3rd Number is the largest");
+            }
+        }
+    }
+}
